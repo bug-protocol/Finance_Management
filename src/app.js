4b91fd491a7809dc35a80056092777ec59c93b4d
bug-protocol <ashutoshm9918@gmail.com>
2026-04-05 19:05:55 +0530

    final placing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
 const apiRateLimiter = require("./middlewares/rateLimiter");
 const errorHandler = require("./middlewares/errorHandler");
@@ -11,12 +12,19 @@ const recordRoutes = require("./routes/recordRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
+const postmanCollectionPath = path.join(
+  __dirname,
+  "docs",
+  "Finance-Dashboard.postman_collection.json"
+);
 
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 app.use(apiRateLimiter);
 
 app.get("/health", (_req, res) => successResponse(res, 200, "Server is healthy"));
+app.get("/docs/postman", (_req, res) => res.sendFile(postmanCollectionPath));
+app.get("/api/v1/docs/postman", (_req, res) => res.sendFile(postmanCollectionPath));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
